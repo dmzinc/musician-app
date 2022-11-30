c294bdd0aeaad02dfc058f26ebbdcaadb9bcb70a
forbes <afolabi2245@gmail.com>
2022-11-30 14:23:55 +0100

    Health check route update2

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {ii
-  res.status('200').send("Status: It's all gjjood!");
+  res.status('200').send("Status: It's all gjjsohod!");
 });
 
 // retrieve all musicians from data store
